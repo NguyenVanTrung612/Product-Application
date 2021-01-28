74e0e6b1e275ce82366c99a0f20659c8e5aeb1e1
NguyenVanTrung612 <vantrung060101@gmail.com>
2021-01-28 12:31:18 +0700

    change

--- Program.cs
+++ Program.cs
@@ -41,8 +41,14 @@ namespace App_Demo
                         Console.WriteLine($"{a} + {b} = { a + b }");
                         break;
 
+                    case 2:
+                        Console.WriteLine("Nhap so nguyen a");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Nhap so nguyen b");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"{a} - {b} = { a - b }");
+                        break;
 
-                   
 
                     default:
                         Console.WriteLine("Nhap cac chuc nang tu 1 den 6!");
